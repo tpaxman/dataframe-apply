cc2c3c697467d4a150a40b3766bc35216914bd52
tpaxman <tpaxman@ualberta.ca>
2022-04-15 23:18:31 -0600

    update comments

--- apply.py
+++ apply.py
@@ -4,6 +4,11 @@ import pandas as pd
 import numpy as np
 
 def apply(func: Callable, df: pd.DataFrame, **kwargs) -> pd.Series:
+    """
+    Applies a vectorized version of a given function (func) to a dataframe (df)
+    where the function inputs are described by a mapping of parameters to
+    argument values (kwargs)
+    """
 
     # ensure that all arguments are actually accepted by the function 
     params_used = set(kwargs)
@@ -13,10 +18,11 @@ def apply(func: Callable, df: pd.DataFrame, **kwargs) -> pd.Series:
     # identify the parameters whose arguments are column names in the DataFrame (which are to be vectorized)
     params_with_colname_args = [param for param, arg in kwargs.items() if arg in df.columns]
 
-    # update the arguments that were column names to instead contain the columns themselves
-    function_inputs = {param: df[arg] if param in params_with_colname_args else arg for param, arg in kwargs.items()}
-
-    # identify parameters that do not have column names as arguments (which are not to be vectorized)
+    # vectorize the function, excluding any parameters not referring to column names
     non_colname_params = set(kwargs).difference(params_with_colname_args)
     vectorized_func = np.vectorize(func, excluded=non_colname_params)
-    return vectorized_func(**function_inputs)
+
+    # update the arguments containing column names to contain the column data and run them in the vectorized function
+    function_inputs = {param: df[arg] if param in params_with_colname_args else arg for param, arg in kwargs.items()}
+    outputs_list = vectorized_func(**function_inputs)
+    return outputs_list
